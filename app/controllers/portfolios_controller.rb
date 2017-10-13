a92fa76a18a889d19c7cb70055f5b5e7f9e350e5
Shawn Kemp <shawn@actionsprout.com>
2017-10-12 20:31:24 -0700

    implemented delete

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -37,8 +37,17 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-  @portfolio_item = Portfolio.find(params[:id])
+   @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+     @portfolio_item.destroy
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'portfolio item was successfully destroyed.' }
+    end
+  end
+
 
 
 end
